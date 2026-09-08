a1ff6488df025a1a049ec96725deb63eb06c0c88
Rishabh Rawat <144614439+Rishabh9560@users.noreply.github.com>
2026-09-08 21:08:13 +0530

    Time: 1 ms (99.68%), Space: 43.2 MB (15.31%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); 
+        for(int ele : stones){
+            pq.add(ele);
+        }
+        while(pq.size() > 1){
+        int x = pq.remove();
+        int y = pq.remove();
+        pq.add(x-y) ;
+        }
+        if(pq.size() == 0) return 0 ;
+        return pq.remove() ;
+
+        
+    }
+}
