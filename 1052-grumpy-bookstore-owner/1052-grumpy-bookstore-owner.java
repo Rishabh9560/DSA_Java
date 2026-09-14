9b8e490f003e93cb59a9dfb0a03e2a6601387591
Rishabh Rawat <144614439+Rishabh9560@users.noreply.github.com>
2026-09-14 12:40:22 +0530

    Time: 3 ms (93.19%), Space: 48.7 MB (56.01%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxSatisfied(int[] arr, int[] grumpy, int k) {
+        int n = arr.length ; 
+        int  i = 0 , j = k-1 , a = i , b = j  ; 
+        int unsatisfied = 0 , maxunsatisfied = 0  ;
+        for(int x = i ; x <= j ; x++){
+            if(grumpy[x] == 1) unsatisfied += arr[x] ;
+        } 
+        while(j < n){
+            if(maxunsatisfied < unsatisfied){
+                maxunsatisfied = unsatisfied ;
+                a = i ; b = j ;
+            }
+            i++ ; j++ ;
+            if(j <n && grumpy[j] == 1) unsatisfied += arr[j] ;
+            if(grumpy[i-1] == 1 ) unsatisfied -= arr[i-1];
+        }
+        for(int x = a  ; x <= b ; x++){
+            grumpy[x] = 0 ; 
+        }
+        int satisfied = 0 ; 
+        for(int x = 0  ; x < n ; x++){
+           if(grumpy[x] == 0) satisfied += arr[x]; 
+        }
+        return satisfied ;
+
+        
+    }
+}
