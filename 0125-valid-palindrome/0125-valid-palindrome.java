1d673447e1a201ee298f086c810e0224e863cb35
Rishabh Rawat <144614439+Rishabh9560@users.noreply.github.com>
2026-09-22 23:05:22 +0530

    Time: 4 ms (43.84%), Space: 44.9 MB (40.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        String valid = "";
+        StringBuilder valid = new StringBuilder();
 
-        // Create valid string
         for (int i = 0; i < s.length(); i++) {
 
             char ch = s.charAt(i);
 
             if (Character.isLetterOrDigit(ch)) {
-                valid += Character.toLowerCase(ch);
+                valid.append(Character.toLowerCase(ch));
             }
         }
-        int i = 0 ; 
-        int j = valid.length()-1 ;
-        while(i <= j ){
-            if(valid.charAt(i) != valid.charAt(j)){
-                return false ;
+
+        int i = 0;
+        int j = valid.length() - 1;
+
+        while (i <= j) {
+
+            if (valid.charAt(i) != valid.charAt(j)) {
+                return false;
             }
-            i++ ;
-            j-- ;
-        } 
-        return true ;       
+
+            i++;
+            j--;
+        }
+
+        return true;
     }
 }
